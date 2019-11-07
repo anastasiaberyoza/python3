781a12e7ef27aeedb10a58a431ceb3ec65dcd909
anastasiaberyoza <56321111+anastasiaberyoza@users.noreply.github.com>
2019-11-07 09:33:52 +0300

    Update string_to_array.py

--- string_to_array.py
+++ string_to_array.py
@@ -7,27 +7,31 @@ def str_to_arr(string):
      
     array = []
     number = 0
-    for i, char in enumerate(string):
+    if len(string) == 0:
+        array = []
+    else:
+        for i, char in enumerate(string):
+            if char.isalpha():
+                # the first char can be the first alphabetical char in the string
+                # if the previous char is not alphabetical, then 
+                # we remember the current char as the first in the alphabetical chain
+                if ((i == 0) or not (string[i-1].isalpha())):
+                    number = i
+                # if char is non-alpha nd its index is more than 0 and the previous one is alpha
+                # Therefore it is the end of the alphabetical substring
+                # If the previous character is non-alpha as well, we move on
+            if not char.isalpha():
+                if ((i > 0) and (string[i-1].isalpha())):
+                    array.append(string[number:i])
+                else:
+                    number = i + 1
+        # The part below adds the last alphabetical substring in the array
+        # if the current char is alphabetical, it means that there is no char after it
+        # But our cycle needs the next non-alpha char so as to add the aplhabetical substring
         if char.isalpha():
-            # the first char can be the first alphabetical char in the string
-            # if the previous char is not alphabetical, then 
-            # we remember the current char as the first in the alphabetical chain
-            if ((i == 0) or not (string[i-1].isalpha())):
-                number = i
-            # if char is non-alpha nd its index is more than 0 and the previous one is alpha
-            # Therefore it is the end of the alphabetical substring
-            # If the previous character is non-alpha as well, we move on
-        else:
-            if ((i > 0) and (string[i-1].isalpha())):
-                array.append(string[number:i])
-            if not (string[i-1].isalpha()):
-                number = i + 1
-    # The part below adds the last alphabetical substring in the array
-    # if the current char is alphabetical, it means that there is no char after it
-    # But our cycle needs the next non-alpha char so as to add the aplhabetical substring
-    if number < len (string):
-        array.append(string[number:])
+            array.append(string[number:i+1])
     return (array)
 print ('Enter string')
 string = input()
 print(str_to_arr(string))
+
