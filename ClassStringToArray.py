325775d870ef9d65ddccd1df4f3163a263ed1094
anastasiaberyoza <56321111+anastasiaberyoza@users.noreply.github.com>
2019-11-07 10:01:16 +0300

    Add files via upload

---
+++ ClassStringToArray.py
@@ -0,0 +1,39 @@
+class Tokenization(object):    
+    #it is not a funtion anymore,it's a method
+    #when we make a new method, we must add one more argument - "self"
+    def str_to_arr(self, string):   
+        
+        array = []
+        number = 0
+        if len(string) == 0:
+            array = []
+        else:
+            for i, char in enumerate(string):
+                if char.isalpha():
+                    # the first char can be the first alphabetical char in the string
+                    # if the previous char is not alphabetical, then 
+                    # we remember the current char as the first in the alphabetical chain
+                    if ((i == 0) or not (string[i-1].isalpha())):
+                        number = i
+                    # if char is non-alpha nd its index is more than 0 and the previous one is alpha
+                    # Therefore it is the end of the alphabetical substring
+                    # If the previous character is non-alpha as well, we move on
+                if not char.isalpha():
+                    if ((i > 0) and (string[i-1].isalpha())):
+                        array.append(string[number:i])
+                    else:
+                        number = i + 1
+            # The part below adds the last alphabetical substring in the array
+            # if the current char is alphabetical, it means that there is no char after it
+            # But our cycle needs the next non-alpha char so as to add the aplhabetical substring
+            if char.isalpha():
+                array.append(string[number:i+1])
+        return (array)
+def main():
+    tokenizate = Tokenization()
+    print ('Enter string')
+    string = input()
+    print(tokenizate.str_to_arr(string))
+    
+if __name__ == '__main__':
+    main()    
