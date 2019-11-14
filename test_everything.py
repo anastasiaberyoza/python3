0e24dbff3d7cc7156a9475e486a0515216ab4067
anastasiaberyoza <56321111+anastasiaberyoza@users.noreply.github.com>
2019-11-14 09:14:38 +0300

    Update test_everything.py

--- test_everything.py
+++ test_everything.py
@@ -1,5 +1,5 @@
 import unittest
-import newday
+import string_to_array
 from Alpha1_str_to_arr import Tokenizer
 
 class TestTokenizer(unittest.TestCase):
@@ -51,43 +51,43 @@ class TestStringMethods(unittest.TestCase):
     
     def test_normal(self):
         s = 'ad b c d'
-        self.assertEqual(newday.str_to_arr(s), ['ad', 'b', 'c', 'd'])
+        self.assertEqual(string_to_array.str_to_arr(s), ['ad', 'b', 'c', 'd'])
         
     def test_with_delimiters(self):
         s = 'ad_b+c-d'
-        self.assertEqual(newday.str_to_arr(s), ['ad', 'b', 'c', 'd'])
+        self.assertEqual(string_to_array.str_to_arr(s), ['ad', 'b', 'c', 'd'])
 
     def test_spaces_only(self):
         s = '     ' #namely 5 spaces
-        self.assertEqual(newday.str_to_arr(s), [])
+        self.assertEqual(string_to_array.str_to_arr(s), [])
 
     def test_spaces_and_alpha(self):
         s = '     a' #5 spaces and alpha
-        self.assertEqual(newday.str_to_arr(s), ['a'])
+        self.assertEqual(string_to_array.str_to_arr(s), ['a'])
 
     def test_spaces_and_number(self):
         s = '     1' #5 spaces and number
-        self.assertEqual(newday.str_to_arr(s), [])
+        self.assertEqual(string_to_array.str_to_arr(s), [])
         
     def test_empty(self):
         s = ''
-        self.assertEqual(newday.str_to_arr(s), [])
+        self.assertEqual(string_to_array.str_to_arr(s), [])
 
     def test_no_spaces(self):
         s = 'abv'
-        self.assertEqual(newday.str_to_arr(s), ['abv'])
+        self.assertEqual(string_to_array.str_to_arr(s), ['abv'])
 
     def test_only_numbers(self):
         s = '123'
-        self.assertEqual(newday.str_to_arr(s), [])
+        self.assertEqual(string_to_array.str_to_arr(s), [])
         
     def test_numbers_and_not_alpha(self):
         s = '123!@#%'
-        self.assertEqual(newday.str_to_arr(s), [])
+        self.assertEqual(string_to_array.str_to_arr(s), [])
         
     def test_number_in_the_end(self):
         s = 'abv3'
-        self.assertEqual(newday.str_to_arr(s), ['abv'])
+        self.assertEqual(string_to_array.str_to_arr(s), ['abv'])
             
 if __name__ == '__main__':
     unittest.main()
